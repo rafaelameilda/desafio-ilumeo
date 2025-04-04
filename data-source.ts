774098055387ce378f4cfa9fc1ca9732a86270ae
rafael-almeida <rafael.alara@bancobbcdigital.com.br>
2025-04-03 22:43:40 -0400

    feat: add data-source and command to migrations

---
+++ data-source.ts
@@ -0,0 +1,16 @@
+import * as dotenv from 'dotenv'
+import { join } from 'path'
+import { DataSource } from 'typeorm'
+
+dotenv.config()
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: [join(__dirname, 'src/**/*.entity.{ts,js}')],
+  migrations: [join(__dirname, 'src/migrations/*.{ts,js}')],
+})
